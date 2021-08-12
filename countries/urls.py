46c3e2fec29681e3c3751c3bc2243c4c1ae45ffb
Oyebamiji Alex <alexferanmi390@gmail.com>
2021-08-12 19:52:40 +0100

    Initial commit

--- countries/urls.py
+++ countries/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import CountriesList, CountriesDetail
 
 urlpatterns = [
-    path('', CountriesList.as_view()),
-    path('countries/<int:pk>/', CountriesDetail.as_view()),
+    # path('', CountriesList.as_view()),
+    path('', CountriesDetail.as_view()),
 ]
